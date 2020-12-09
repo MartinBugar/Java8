866aad94347a03e33e4de80c4aad8475247ec90e
prometheus <martin1.bugar@gmail.com>
2020-12-09 11:34:37 +0100

    forrmatovaie2

--- src/com/martyx/Strings.java
+++ src/com/martyx/Strings.java
@@ -1,7 +1,21 @@
 package com.martyx;
 
+import java.util.Calendar;
+import java.util.Locale;
+
 public class Strings {
     public static void main(String[] args) {
+       // formatovanie();
+        advancedFormatovanie();
+    }
+
+    private static void advancedFormatovanie() {
+        Calendar calendar = Calendar.getInstance(Locale.forLanguageTag("sk_SK"));
+        String s1 = String.format("Matove narodky  : %1$td %1$tb %1$ty", calendar);
+        System.out.println(s1);
+    }
+
+    private static void formatovanie() {
 
         int i = 2018;
         String meno = "martin";
