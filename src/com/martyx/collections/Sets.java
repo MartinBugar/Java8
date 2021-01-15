7d486e3c64025428636e14b3a231ad5135ca7d9d
prometheus9911 <martin1.bugar@gmail.com>
2021-01-15 13:28:31 +0100

    kolekcia set

--- src/com/martyx/collections/Sets.java
+++ src/com/martyx/collections/Sets.java
@@ -1,7 +1,32 @@
 package com.martyx.collections;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Sets {
     public static void main(String[] args) {
 
+        Set<String> set1 = new HashSet<>();
+        set1.add("element1");
+        set1.add("element2");
+        set1.add("element3");
+        set1.add("element4");
+
+        Set<String> set2 = new HashSet<>();
+        set2.add("element3");
+        set2.add("element4");
+        set2.add("element5");
+        set2.add("element6");
+
+        printSet(set1);
+        printSet(set2);
+
+    }
+
+    public static void printSet (Set<String> set){
+        for (String s : set ){
+            System.out.println(s);
+        }
+        System.out.println();
     }
 }
